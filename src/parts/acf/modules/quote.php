504108ff3f26d57c3cdfd1462f54a856dddb347e
mattwills8 <matt_wills8@hotmail.co.uk>
2019-01-24 13:23:41 +0000

    add quote section styling

---
+++ src/parts/acf/modules/quote.php
@@ -0,0 +1,8 @@
+<div class="row quote-wrapper" style="background-image: url('<?php echo $background_image; ?>');">
+  <h2 class="quote" >
+    <span class="quote-mark">"</span> <?php echo $quote; ?> <span class="quote-mark">"</span>
+  </h2>
+  <div class="quote-author">
+    - <?php echo $quote_author; ?>
+  </div>
+</div>
