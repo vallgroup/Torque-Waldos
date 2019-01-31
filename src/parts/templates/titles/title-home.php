5cc3258f902b4e080d5c405ccbdea3cd7e46ba17
mattwills8 <matt_wills8@hotmail.co.uk>
2019-01-31 15:18:44 -0500

    add new page hero

--- src/parts/templates/titles/title-home.php
+++ src/parts/templates/titles/title-home.php
@@ -1,12 +1,6 @@
 <?php
 
-require_once( get_template_directory() . '/includes/customizer/customizer-tabs/tabs/torque-customizer-tab-site-identity-class.php' );
-
-$tab_settings = Torque_Customizer_Tab_Site_Identity::get_settings();
-
-$logo_src = get_theme_mod( $tab_settings['logo_white_setting'] );
-
-$tagline = get_bloginfo( 'description' );
+$tagline = html_entity_decode(get_bloginfo( 'description' ));
 
 $image_url = get_the_post_thumbnail_url();
 
@@ -16,14 +10,10 @@ if ($image_url) {
 
 <div class="hero" style="background-image: url('<?php echo $image_url; ?>');">
 
-  <?php if ($logo_src) { ?>
-    <img class="logo-white" src="<?php echo $logo_src; ?>" />
-  <?php } ?>
-
   <?php if ($tagline) { ?>
-    <h3 class="tagline" >
+    <h1>
       <?php echo $tagline; ?>
-    </h3>
+    </h1>
   <?php } ?>
 
 </div>
