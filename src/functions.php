d3d05357b9e8b6ca1166956fb7065a4a35ffbf76
mattwills8 <matt_wills8@hotmail.co.uk>
2019-02-12 16:32:03 -0600

    hide irrelevant admin menu pages

--- src/functions.php
+++ src/functions.php
@@ -39,10 +39,10 @@ if ( class_exists( 'Waldos_Widgets' ) ) {
  function torque_remove_menus(){
 
    //remove_menu_page( 'index.php' );                  //Dashboard
-   //remove_menu_page( 'edit.php' );                   //Posts
+   remove_menu_page( 'edit.php' );                   //Posts
    //remove_menu_page( 'upload.php' );                 //Media
    //remove_menu_page( 'edit.php?post_type=page' );    //Pages
-   //remove_menu_page( 'edit-comments.php' );          //Comments
+   remove_menu_page( 'edit-comments.php' );          //Comments
    //remove_menu_page( 'themes.php' );                 //Appearance
    //remove_menu_page( 'plugins.php' );                //Plugins
    //remove_menu_page( 'users.php' );                  //Users
