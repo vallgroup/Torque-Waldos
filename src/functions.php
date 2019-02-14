b9ad375e5ac9c6e30fd518f807552db6ea04a403
mattwills8 <matt_wills8@hotmail.co.uk>
2019-02-14 13:11:13 -0600

    add GA tag

--- src/functions.php
+++ src/functions.php
@@ -91,4 +91,19 @@ function torque_enqueue_child_scripts() {
     );
 }
 
+add_action('wp_head', 'tq_hook_gtm', 0);
+function tq_hook_gtm() {
+?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134516939-1"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-134516939-1');
+  </script>
+<?php
+}
+
 ?>
